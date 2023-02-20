0014bb13672f9d560871c2ff1a4ba52f65d004c7
rysonw <rysonplays@gmail.com>
2023-02-19 14:31:46 -1000

    Final Changes

--- week_4/map_comparison/map_comparison.cc
+++ week_4/map_comparison/map_comparison.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-
+#include <map>
+#include <unordered_map>
 /////////////////////////////////////////
 // INCLUDE NECESSARY HEADER FILES HERE //
 /////////////////////////////////////////
@@ -15,17 +16,28 @@ void func_map (vector<pair<int,string>>& items)
   // CREATE AN EMPTY map MAPPING int TO string. //
   ////////////////////////////////////////////////
 
+  std::map<int, std::string> thisMap;
+
 
   ////////////////////////////////////////////
   // USING A RANGE-BASED for(...) LOOP,     //
   // INSERT EACH ITEM IN items INTO THE MAP //
   ////////////////////////////////////////////
 
+  for (auto const& item : items) {
+    thisMap[item.first] = item.second;
+  }
+
 
   //////////////////////////////////////////////
   // USING A RANGE-BASED for(...) LOOP,       //
   // PRINT OUT EACH KEY-VALUE PAIR IN THE MAP //
   //////////////////////////////////////////////
+
+    for (auto const& [key, value] : thisMap) {
+        std::cout << "Key: " << key << ", Value: " << value << std::endl;
+  }
+
 
 
   cout << endl << "=== END func_map ===" << endl;
@@ -38,6 +50,8 @@ void func_unordered_map (vector<pair<int,string>>& items)
   //////////////////////////////////////////////////////////
   // CREATE AN EMPTY unordered_map MAPPING int TO string. //
   //////////////////////////////////////////////////////////
+  
+  std::unordered_map<int, std::string> thisUnorderedMap;
 
 
   /////////////////////////////////////////////
@@ -50,6 +64,15 @@ void func_unordered_map (vector<pair<int,string>>& items)
   // IN THE FOLLOWING FORMAT:                //
   //   o  "[N,B,LF] = [3,10,0.4432]"         //
   /////////////////////////////////////////////
+  
+    for (auto const& item : items) {
+    thisUnorderedMap[item.first] = item.second;
+    
+  std::cout << "[" << thisUnorderedMap.size() << ", " << thisUnorderedMap.bucket_count() << ", "
+  << thisUnorderedMap.load_factor() << "]" << std::endl;
+  }
+  
+  
 
 
   ///////////////////////////////////////////////
@@ -85,4 +108,5 @@ int main ()
 
   return 0;
 }
+
 

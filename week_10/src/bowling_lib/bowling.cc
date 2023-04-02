01b53e8d5bef83304e17b03ec86fe86ea0c7d0ea
rysonw <rysonplays@gmail.com>
2023-04-02 01:35:42 -1000

    Added spare and strike carryover

--- week_10/src/bowling_lib/bowling.cc
+++ week_10/src/bowling_lib/bowling.cc
@@ -39,30 +39,73 @@ namespace Bowling
         }
     }
 
+    void calculate_carryover(const std::vector<std::vector<int>>& frames, std::vector<int>& carryover) {
+      int num_frames = frames.size();
+
+      // Calculate carryover for spares
+      for (int i = 0; i < num_frames - 1; i++) {
+          if (frames[i][0] + frames[i][1] == 10) { // Spare
+              carryover[i] = frames[i+1][0];
+          }
+      }
+
+      // Calculate carryover for strikes
+      for (int i = 0; i < num_frames - 2; i++) {
+          if (frames[i][0] == 10) { // Strike
+              if (frames[i+1][0] == 10) { // Double
+                  carryover[i] = frames[i+1][0] + frames[i+2][0];
+              } else { // Single
+                  carryover[i] = frames[i+1][0] + frames[i+1][1];
+              }
+          }
+      }
+  }
+
     void Game::record_ball(int num_pins) {
       if (is_complete()) {
         throw std::runtime_error("Game has already finished");
       }
 
       if (num_pins < 0 || num_pins > 10) {
-        throw std::invalid_argument("Invalid input");
+          throw std::invalid_argument("Invalid input");
       }
 
       frames[current_frame - 1][current_ball - 1] = num_pins;
-      bool is_frame_complete = false;
-      
-      if (Game::get_curr_ball() == 1 && num_pins < 10) { //roll was not a strike
-          current_ball++;
-      }
-      else {
-        current_ball = 1;
-        current_frame++;
-      }
-    }
 
-    int player_roll() {
-        int pinsHitThisRoll = 0;
-        int frame_index = 0;
-    }
+      // Check if the frame is complete
+      bool frame_complete = false;
+      if (current_frame == 10) {
+          // Last frame
+          if (current_ball == 3 || (current_ball == 2 && (frames[9][0] + frames[9][1] < 10))) {
+              frame_complete = true;
+          }
+      } else {
+          // Regular frame
+          if (current_ball == 2 || frames[current_frame - 1][0] == 10) {
+              frame_complete = true;
+          }
+      }
+
+      if (frame_complete) {
+          // Calculate carryover and update score for completed frame
+          std::vector<int> carryover(10, 0); // Initialize carryover vector to all zeros
+          int frame_score = calculate_frame_score(frames[current_frame - 1], carryover, current_frame - 1);
+          total_score += frame_score;
+          // Add carryover to previous frames
+          for (int i = current_frame - 2; i >= 0 && carryover[i] > 0; i--) {
+              total_score += carryover[i];
+          }
+          // Reset current ball and frame
+          current_ball = 1;
+          current_frame++;
+      } else {
+          // Update current ball
+          if (current_ball == 1 && num_pins < 10) {
+              current_ball++;
+          } else {
+              current_ball = 1;
+              current_frame++;
+          }
+      }
+  }
 }
-
